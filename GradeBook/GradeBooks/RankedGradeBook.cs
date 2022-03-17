c12204cbc4adff0d9a8bacda2658ceee4ab9151d
Dawid Migdał <dejf026@gmail.com>
2022-03-17 19:16:37 +0100

    Creating the RankedGradeBook class

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class RankedGradeBook:BaseGradeBook
+    {
+
+        public RankedGradeBook(string name) :base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+
+
+
+    }
+
+
+
+}
+
