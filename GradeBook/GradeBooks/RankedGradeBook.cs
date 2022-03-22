1f6a1894171846c6588aced8505b86c448c8fd22
Dawid Migdał <dejf026@gmail.com>
2022-03-22 19:58:56 +0100

    Update BaseGradeBook.GetGPA and  Update HelpCommand

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -6,10 +6,10 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    class RankedGradeBook : BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
 
-        public RankedGradeBook(string name) : base(name)
+        public RankedGradeBook(string name, bool isWeighted) : base(name, isWeighted)
         {
             Type = Enums.GradeBookType.Ranked;
         }
@@ -19,7 +19,7 @@ namespace GradeBook.GradeBooks
 
             if (Students.Count < 5)
             {
-                throw new InvalidOperationException("Less than 5 students");
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
 
             }
             var threshold = (int)Math.Ceiling(Students.Count * 0.2);
@@ -52,13 +52,14 @@ namespace GradeBook.GradeBooks
                 return 'F';
             }
         }
+        
 
         public override void CalculateStatistics()
         {
 
             if (Students.Count < 5)
             {
-                Console.WriteLine("Ranked grading requires at least 5 students");
+                Console.WriteLine("Ranked grading requires at least 5 students.");
                 return;
             }
 
@@ -69,7 +70,7 @@ namespace GradeBook.GradeBooks
         {
             if (Students.Count < 5)
             {
-                Console.WriteLine("Ranked grading requires at least 5 students");
+                Console.WriteLine("Ranked grading requires at least 5 students.");
                 return;
             }
 
